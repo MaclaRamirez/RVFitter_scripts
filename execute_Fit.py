78c107545806395afe28e6807c340089653f5626
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-04-29 00:04:39 +0200

    Updates fitting skeleton

--- execute_Fit.py
+++ execute_Fit.py
@@ -35,43 +35,44 @@ def main(args):
     myfitter.load_df(filename=filename)
 
     collected_fitters = []
-    for shape in ["gaussian"]:
-        this_fitter = fit_without_constraints(myfitter, shape=shape)
-        this_fitter.label = "without constraints"
+    for shape_profile in ["gaussian", "lorentzian"]:
+        this_fitter = fit_without_constraints(myfitter, shape_profile=shape_profile)
+        this_fitter.label = shape_profile + " without constraints"
         collected_fitters.append(this_fitter)
         #  myfitter.plot_fit()
         #  myfitter.plot_residuals()
         #  myfitter.plot_fit_with_residuals()
 
-        this_fitter = fit_with_constraints(myfitter, shape=shape)
-        this_fitter.label = "with constraints"
+        this_fitter = fit_with_constraints(myfitter, shape_profile=shape_profile)
+        this_fitter.label = shape_profile + " with constraints"
         collected_fitters.append(this_fitter)
 
-
-    color_dict = {0: "red", 1: "blue"}
+    color_dict = {0: "red", 1: "blue", 2: "green", 3: "orange"}
     fig, axes = myfitter.get_fig_and_axes()
     for idx, this_fitter in enumerate(collected_fitters):
         if idx == 0:
             this_fitter.plot_data(fig=fig, axes=axes)
         this_fitter.plot_fit(fig=fig, axes=axes, plot_dict={"zorder": 2.5, "color": color_dict[idx], "label": this_fitter.label})
-    myfitter.apply_legend(axes=axes)
+    handles, labels = axes[-1, -1].get_legend_handles_labels()
+    fig.legend(handles, labels, ncol=2, loc='lower center')
+    fig.savefig("test_constraints.pdf")
     plt.show()
 
 
-def fit_with_constraints(myfitter, shape="gaussian"):
+def fit_with_constraints(myfitter, shape_profile="gaussian"):
     # prepare fitting
     this_fitter = copy.deepcopy(myfitter)
-    this_fitter.shape = shape
+    this_fitter.shape_profile = shape_profile
     this_fitter.constrain_parameters(group="cen", constraint_type="epoch")
     this_fitter.constrain_parameters(group="amp", constraint_type="line_profile")
     this_fitter.constrain_parameters(group="sig", constraint_type="line_profile")
     this_fitter.run_fit()
     return this_fitter
 
-def fit_without_constraints(myfitter, shape="gaussian"):
+def fit_without_constraints(myfitter, shape_profile="gaussian"):
     # prepare fitting
     this_fitter = copy.deepcopy(myfitter)
-    this_fitter.shape = shape
+    this_fitter.shape_profile = shape_profile
     this_fitter.run_fit()
     return this_fitter
 
