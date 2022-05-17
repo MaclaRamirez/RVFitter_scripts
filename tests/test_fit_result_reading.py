0d00073b144bcef1af137f10d39e685b2b28d089
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-05-17 20:55:39 +0200

    Adding function for comparing different fit-results

--- tests/test_fit_result_reading.py
+++ tests/test_fit_result_reading.py
@@ -8,6 +8,7 @@ import os
 import copy
 import sys
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_tmp_file(filename):
     with open(filename, "r") as f:
@@ -37,64 +38,131 @@ class TestRVFitter(unittest.TestCase):
             line_list=line_list)
         self.myfitter = myfitter
 
-    def test_fitting(self):
+    #  def test_fitting(self):
+    #
+    #      filename = os.path.join(os.path.dirname(self.specsfilelist),
+    #                              "B275_speclist.pkl")
+    #      self.myfitter = RVFitter.load_from_df_file(filename=filename)
+    #
+    #      collected_fitters = []
+    #      for shape_profile in ["gaussian", "lorentzian"]:
+    #          this_fitter = fit_without_constraints(self.myfitter, shape_profile=shape_profile)
+    #          this_fitter.label = shape_profile + " without constraints"
+    #          collected_fitters.append(this_fitter)
+    #
+    #          this_fitter = fit_with_constraints(self.myfitter, shape_profile=shape_profile)
+    #          this_fitter.label = shape_profile + " with constraints"
+    #          collected_fitters.append(this_fitter)
+    #
+    #      color_dict = {0: "red", 1: "blue", 2: "green", 3: "orange"}
+    #      fig, axes = self.myfitter.get_fig_and_axes()
+    #      for idx, this_fitter in enumerate(collected_fitters):
+    #          if idx == 0:
+    #              this_fitter.plot_data(fig=fig, axes=axes)
+    #          this_fitter.plot_fit(fig=fig, axes=axes, plot_dict={"zorder": 2.5, "color": color_dict[idx], "label": this_fitter.label})
+    #      handles, labels = axes[-1, -1].get_legend_handles_labels()
+    #      fig.legend(handles, labels, ncol=2, loc='lower center')
+    #      fig.savefig("test_constraints.pdf")
+    #      # plt.show()
+    #
+    #      #Compare RV measurements
+    #      dates = self.myfitter.df['date'].unique()
+    #      date = dates[0]
+    #      # line_hashes = this_epoch['line_hash'].unique()
+    #      fig, ax = plt.subplots()
+    #      colors = ["red", "blue", "green"]
+    #      for j, date in enumerate(dates):
+    #          this_epoch = self.myfitter.df.query("date == '{}'".format(date))
+    #          my_dict = {}
+    #          for fitter in collected_fitters:
+    #              print(fitter.label)
+    #              my_dict[fitter.label] = {}
+    #              for idx, row in this_epoch.iterrows():
+    #                  hash = row['line_hash']
+    #                  variable_formater = 'cen_line_{}_epoch_{}'.format(hash, date)
+    #                  print(variable_formater)
+    #                  print(fitter.result.params[variable_formater].value)
+    #                  my_dict[fitter.label][hash] = fitter.result.params[variable_formater].value
+    #
+    #
+    #          ax.plot(list(my_dict['gaussian without constraints'].values()),
+    #                  list(my_dict['lorentzian without constraints'].values()), 'o', color=colors[j])#, label='without constraints')
+    #          ax.plot(np.linspace(-1, 60, 1000), np.linspace(-1, 60, 1000), '--',  color=colors[j], label=date)
+    #          ax.plot(list(my_dict['gaussian with constraints'].values()),
+    #                  list(my_dict['lorentzian with constraints'].values()), 'o',  color=colors[j], alpha=0.05)# label='with constraints')
+    #
+    #      ax.legend()
+    #      #  plt.show()
+
+
+    def test_table(self):
 
         filename = os.path.join(os.path.dirname(self.specsfilelist),
                                 "B275_speclist.pkl")
-        self.myfitter.load_df(filename=filename)
+        self.myfitter = RVFitter.load_from_df_file(filename=filename)
 
-        collected_fitters = []
+        collected_fitters = {}
         for shape_profile in ["gaussian", "lorentzian"]:
             this_fitter = fit_without_constraints(self.myfitter, shape_profile=shape_profile)
             this_fitter.label = shape_profile + " without constraints"
-            collected_fitters.append(this_fitter)
-            #  myfitter.plot_fit()
-            #  myfitter.plot_residuals()
-            #  myfitter.plot_fit_with_residuals()
+            collected_fitters["no_constraint_" + shape_profile] = this_fitter
 
             this_fitter = fit_with_constraints(self.myfitter, shape_profile=shape_profile)
             this_fitter.label = shape_profile + " with constraints"
-            collected_fitters.append(this_fitter)
-
-        color_dict = {0: "red", 1: "blue", 2: "green", 3: "orange"}
-        fig, axes = self.myfitter.get_fig_and_axes()
-        for idx, this_fitter in enumerate(collected_fitters):
-            if idx == 0:
-                this_fitter.plot_data(fig=fig, axes=axes)
-            this_fitter.plot_fit(fig=fig, axes=axes, plot_dict={"zorder": 2.5, "color": color_dict[idx], "label": this_fitter.label})
-        handles, labels = axes[-1, -1].get_legend_handles_labels()
-        fig.legend(handles, labels, ncol=2, loc='lower center')
-        fig.savefig("test_constraints.pdf")
-        # plt.show()
-
-        #Compare RV measurements
-        dates = self.myfitter.df['date'].unique()
-        date = dates[0]
-        # line_hashes = this_epoch['line_hash'].unique()
-        fig, ax = plt.subplots()
-        colors = ["red", "blue", "green"]
-        for j, date in enumerate(dates):
-            this_epoch = self.myfitter.df.query("date == '{}'".format(date))
-            my_dict = {}
-            for fitter in collected_fitters:
-                print(fitter.label)
-                my_dict[fitter.label] = {}
-                for idx, row in this_epoch.iterrows():
-                    hash = row['line_hash']
-                    variable_formater = 'cen_line_{}_epoch_{}'.format(hash, date)
-                    print(variable_formater)
-                    print(fitter.result.params[variable_formater].value)
-                    my_dict[fitter.label][hash] = fitter.result.params[variable_formater].value
+            collected_fitters["constraint_" + shape_profile] = this_fitter
 
 
-            ax.plot(list(my_dict['gaussian without constraints'].values()),
-                    list(my_dict['lorentzian without constraints'].values()), 'o', color=colors[j])#, label='without constraints')
-            ax.plot(np.linspace(-1, 60, 1000), np.linspace(-1, 60, 1000), '--',  color=colors[j], label=date)
-            ax.plot(list(my_dict['gaussian with constraints'].values()),
-                    list(my_dict['lorentzian with constraints'].values()), 'o',  color=colors[j], alpha=0.05)# label='with constraints')
 
-        ax.legend()
-        plt.show()
+
+        # create dataframe from collected_fitters
+        df = pd.DataFrame()
+        for key, fitter in collected_fitters.items():
+            df = add_parameters_to_df(df=df, fitter=fitter, suffix="_" + key)
+        print(df)
+
+def add_parameters_to_df(df, fitter, suffix):
+    l_amp = []
+    l_cen = []
+    l_sig = []
+    l_error_amp = []
+    l_error_cen = []
+    l_error_sig = []
+    for parameter in fitter.df["parameters"]:
+        amp = parameter["amp"]
+        cen = parameter["cen"]
+        sig = parameter["sig"]
+
+        l_amp.append(fitter.result.params[amp].value)
+        l_cen.append(fitter.result.params[cen].value)
+        l_sig.append(fitter.result.params[sig].value)
+
+        l_error_amp.append(fitter.result.params[amp].stderr)
+        l_error_cen.append(fitter.result.params[cen].stderr)
+        l_error_sig.append(fitter.result.params[sig].stderr)
+
+    this_df = pd.DataFrame({"date" + suffix: fitter.df["date"],
+        "line_profile" + suffix: fitter.df["line_profile"],
+        "amp" + suffix: l_amp,
+        "cen" + suffix: l_cen,
+        "sig" + suffix: l_sig,
+        "error_amp" + suffix: l_error_amp,
+        "error_cen" + suffix: l_error_cen,
+        "error_sig" + suffix: l_error_sig
+        })
+
+    for col in df.columns:
+        if "date" in col:
+            check = df[col] == this_df["date" + suffix]
+            if np.sum(check) != len(check):
+                raise Exception("Error: date mismatch")
+        if "line_profile" in col:
+            check = df[col] == this_df["line_profile" + suffix]
+            if np.sum(check) != len(check):
+                raise Exception("Error: line_profile mismatch")
+    df = pd.concat([df, this_df], axis=1)
+    return df
+
+
 
 def fit_with_constraints(myfitter, shape_profile="gaussian"):
     # prepare fitting
